ca72e5c83a57e5a0383c6c2a80f1f65a605ae2bf
samsondanicah <danicahsamson26@gmail.com>
2023-06-26 15:22:42 +0800

    new

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  get 'welcome/Controller'
+  get 'welcome/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root 'welcome#index'
+
 end
+
